12062655e823c0f230acf3f316f8e5996256b420
Tony Sorensen <tony48853@gmail.com>
2021-05-05 23:28:25 -0700

    post/put/delete done

--- api/server.js
+++ api/server.js
@@ -40,9 +40,63 @@ server.get("/api/users/:id", async (req, res) => {
 });
 
 //POST /api/users - creates user with info sent inside request body
-//
+server.post("/api/users", async (req, res) => {
+  try {
+    const newUser = req.body;
+    if (!newUser.name || !newUser.bio) {
+      res.status(400).json({
+        message: "Please provide name and bio for the user",
+      });
+    } else {
+      const addUser = await Users.create(newUser);
+      res.status(201).json(addUser);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "There was an error while saving the user to the database",
+    });
+  }
+});
 //PUT /api/users/:id - Updates the user with the specified id using data from request body and returns the modified user
-//
+server.put("/api/users/:id", async (req, res) => {
+  try {
+    if (!req.body.name || !req.body.bio) {
+      res.status(400).json({
+        message: "Please provide name and bio for the user",
+      });
+    } else {
+      const updatedUser = await Users.update(req.params.id, req.body);
+      if (!updatedUser) {
+        res.status(404).json({
+          message: "The user with the specified ID does not exist",
+        });
+      } else {
+        res.json(updatedUser);
+      }
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The user information could not be modified",
+    });
+  }
+});
 //DELETE /api/users/:id - Removes hte user with the specified id and returns the deleted user
-
+server.delete("/api/users/:id", (req, res) => {
+  Users.delete(req.params.id)
+    .then((deletedUser) => {
+      if (!deletedUser) {
+        res.status(404).json({
+          message: "The user with the specified ID does not exist",
+        });
+      } else {
+        res.json(deletedUser);
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "The user could not be removed",
+        error: err,
+      });
+    });
+});
 module.exports = server; // EXPORT YOUR SERVER instead of {}
